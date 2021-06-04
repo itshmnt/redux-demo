cfe6a148b1a0cb75748b4919598f13b459e54550
itshmnt <hemantxs5@gmail.com>
2021-06-04 13:58:29 +0530

    Setting up Subscription

--- redux-basics.js
+++ redux-basics.js
@@ -28,9 +28,13 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+// Subscription
+// it is called automatically each time whenever the action dispatches.
+store.subscribe(() => {
+  console.log('[Subscription]', store.getState());
+});
+
 // Dispatching Action
 store.dispatch({type: 'INC_COUNTER'})
 store.dispatch({type: 'ADD_COUNTER', value: 10})
 console.log(store.getState());
-
-// Subscription
